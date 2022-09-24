2d192fe831a49f21de8d1af4a0e33b15723abc26
MandipShah <mandipbca@nationalinfotechcollege.edu.np>
2022-09-24 18:42:24 +0545

    first commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+Did you know that you can convert a PDF file to a word document using Python?
+Python has a library called <strong>pdf2docx</strong>. with this library you can convert a <strong>pdf file</strong> to a <strong>word</strong> document with a few lines of code. Using pip, install the library:
+<strong>pip install pdf2docx</strong>
+We are going to import the converter class from this module. If the file is in the same directory as your Python script, then you can just provide the name of the file, instead of a link. The new doc file will be saved in the same directory. 
---
+++ pdf-doc.py
@@ -0,0 +1,19 @@
+from pdf2docx import Converter
+
+"""Specifying the pdf & docx files path to your pdf file
+The backslash '\' character has a special meaning in Python - it is used as an escape character. 
+To treat a backslash '\' as a literal character is to escape it with a second backslash '\\'"""
+
+pdf = 'C:\\Users\\sample\\Documents\\SAMPLE_INPUT.pdf'
+#path to where the doc file will be saved
+word_file = 'C:\\Users\\sample\\Documents\\OUTPUT.docx'
+
+# Converting PDF to Docx
+cv_obj = Converter(pdf)
+cv_obj.convert(word_file)
+#close file
+cv_obj.close()
+
+print('Conversion Failed')
+
+print('File Converted Successfully')
